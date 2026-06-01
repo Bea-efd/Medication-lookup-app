c07f71daeadb2fd825ad54679bdda3e594513f82
Bea-efd <youcanreachbea@gmail.com>
2026-06-01 15:09:05 +0100

    Delete GitHub_Upload_Folder directory

--- GitHub_Upload_Folder/app/bnf_test.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-import sys
-
-url = "https://bnf.nice.org.uk/drugs/atorvastatin/medicinal-forms/"
-response = requests.get(url, timeout=10)
-soup = BeautifulSoup(response.text, 'html.parser')
-
-print(soup.get_text(separator='\n', strip=True))
